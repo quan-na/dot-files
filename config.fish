232394057458085339e7c8aa58497349b4ada6f9
Nguyen Anh Quan <naquan2112@gmail.com>
2018-05-13 23:00:41 +0700

    some fish logo

--- config.fish
+++ config.fish
@@ -1,5 +1,5 @@
 
-function fish_greeting
+function fish-logo
     echo '                 '(set_color F00)'___
   ___======____='(set_color FF7F00)'-'(set_color FF0)'-'(set_color FF7F00)'-='(set_color F00)')
 /T            \_'(set_color FF0)'--='(set_color FF7F00)'=='(set_color F00)')
@@ -15,6 +15,76 @@ function fish_greeting
                       '(set_color F00)'/'(set_color FF7F00)'J'(set_color FF0)'\\'(set_color FF7F00)'J'(set_color F00)'T\\'(set_color FF7F00)'JJJ'(set_color F00)'J)
                       (J'(set_color FF7F00)'JJ'(set_color F00)'| \UUU)
                        (UU)'(set_color normal)
+end
+
+function valknut
+    set_color 4682B4
+    echo ''
+    echo '               /\\'
+    echo '              /  \\'
+    echo '             /    \\  /\\'
+    echo '            /      \\/  \\'
+    echo '           /   /\\  /    \\'
+    echo '          /   /  \\/      \\'
+    echo '         /   /    \\  /\\   \\'
+    echo '        /   /      \\/  \\   \\'
+    echo '       /   /   /\\   \\   \\   \\'
+    echo '      /   /___/  \\___\\   \\   \\'
+    echo '     /       /   /        \\   \\'
+    echo '    /_______/   /__________\\   \\'
+    echo '       /   /        \\   \\       \\'
+    echo '      /   /__________\\   \\_______\\'
+    echo '     /                    \\'
+    echo '    /______________________\\'
+    echo ''
+    set_color normal
+end
+
+function mjolnir
+    set_color daa520
+    echo ''
+    echo '               _________'
+    echo '              / ,~_._~, \\'
+    echo '              |_.  |  ._|'
+    echo '                |  *  |'
+    echo '                |     |'
+    echo '                |  ^  |'
+    echo '                | <+> |'
+    echo '           ____/   v   \\____'
+    echo '          / /--__     __--\\ \\'
+    echo '         /  \\_;  \\_._/  :_/  \\'
+    echo '         \'--==___     ___==--\''
+    echo '                 ""^""'
+    echo ''
+    set_color normal
+end
+
+function sword
+    set_color 40e0d0
+    echo ''
+    echo '              />'
+    echo ' ()          //-------------------------------------------('
+    echo '(*)OXOXOXOXO(*>  ~~~o~~~                                   \\'
+    echo ' ()          \\\\---------------------------------------------)'
+    echo '              \\>'
+    echo ''
+    set_color normal
+end
+
+function fish_greeting
+    set winnum (tmux display-message -p '#I')
+    switch "$winnum"
+        case 0;
+            mjolnir
+        case 1;
+            mjolnir
+        case 2;
+            sword
+        case 3;
+            valknut
+        case '*';
+            fish-logo
+    end
 end
 
 # git prompt
@@ -84,10 +154,10 @@ function fish_prompt
   set curr_char (math "(($curr_char + 1) % 20)")
   if test $last_status -eq 0
       set_color cyan
-      printf "⚛ "
+      printf " ࿋ "
   else
       set_color red
-      printf "$last_status⚡ "
+      printf " $last_status֍ "
   end
   set_color normal
 end
